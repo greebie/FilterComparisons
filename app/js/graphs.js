f17f35787a8b74331dbee5b715cc4e0a0b20512b
Ryan Deschamps <ryan.deschamps@gmail.com>
2018-02-15 12:54:24 -0500

    Add demo GEXFs.  First commit.

---
+++ app/js/graphs.js
@@ -0,0 +1,12 @@
+sigma.parsers.gexf(
+    'GEXF/cpp210Degree.gexf',
+    { // Here is the ID of the DOM element that
+      // will contain the graph:
+      container: 'cpp2-10'
+    },
+    function(s) {
+      // This function will be executed when the
+      // graph is displayed, with "s" the related
+      // sigma instance.
+    }
+  );
