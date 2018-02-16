ef9952455c53cdaf81813776ac11af400e9fd4f5
Ryan Deschamps <ryan.deschamps@gmail.com>
2018-02-16 15:56:03 -0500

    Add the original graph to the set.

--- app/js/graphs.js
+++ app/js/graphs.js
@@ -2,6 +2,14 @@ var s = new sigma('cppdegree');
 var s2 = new sigma('cppbetween');
 var s3 = new sigma('cppeigen');
 var s4 = new sigma('cppauthority');
+var so = new sigma('cpp');
+sigma.parsers.gexf(
+  'GEXF/cpp2.gexf',
+  so,
+  function(y) {
+  so.refresh();
+  }
+);
 sigma.parsers.gexf(
     'GEXF/cpp225Degree.gexf',
     s,
